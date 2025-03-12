36f82e6c44f74f388f2f7dc6d2ddfb49e887001f
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2025-03-12 13:26:49 +0500

    Quiz Solved

--- quiz.dart
+++ quiz.dart
@@ -1,3 +1,93 @@
+import 'dart:io';
+
 void main(){
-  print("main asasdas");
+  ShoppingCart cart=new ShoppingCart();
+  cart.showMenu();
+}
+
+class Product{
+  String name;
+  double price;
+
+  Product(this.name,this.price);
+}
+
+class ShoppingCart{
+  List<Product> products=[
+    Product("Laptop",999),
+    Product("Phone",999),
+    Product("Android",999)
+  ];
+
+  List<Product> cart=[];
+
+  void showMenu(){
+    bool showMenuBool=true;
+    while(showMenuBool) {
+      print("1.View Products");
+      print("2.Add to Cart");
+      print("3.View Cart");
+      print("4.Exit");
+
+      String? choice = stdin.readLineSync();
+
+      switch (choice) {
+        case "1":
+          this.viewItems();
+          break;
+
+        case "2":
+          this.addToCart();
+          break;
+
+        case "3":
+          this.viewCart();
+          break;
+
+        case "4":
+          showMenuBool=false;
+          break;
+
+        default:
+          print("Invalid Input");
+          break;
+      }
+    }
+  }
+
+  void viewItems(){
+    int count=0;
+    for(var product in products){
+      print("${count}.Name : ${product.name}  Price : ${product.price}");
+      count++;
+    }
+  }
+
+  void addToCart(){
+    this.viewItems();
+    String? input = stdin.readLineSync();
+    int choice=int.tryParse(input ?? "")??0;
+
+    if(choice < 0 || choice>products.length-1){
+      print("Invalid Input");
+      return;
+    }
+
+    cart.add(products[choice]);
+  }
+
+  void viewCart(){
+    if(cart.isEmpty){
+      print("Empty Cart");
+      return;
+    }
+    int count=0;
+    double sum=0;
+    for(var product in cart){
+      print("${count}.Name : ${product.name}  Price : ${product.price}");
+      count++;
+      sum+=product.price;
+    }
+    print("Total Cart Price: $sum");
+  }
 }
